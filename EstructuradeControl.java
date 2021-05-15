3b55ddc0a21f53ad704a82319825d8e0d3d67fe3
Daniel Canedo <dnl.canedo@gmail.com>
2021-05-14 20:07:43 -0400

    Corrección de identación

--- EstructuradeControl.java
+++ EstructuradeControl.java
@@ -5,13 +5,10 @@ public class EstructuradeControl {
         if(num ==1) {
             System.out.println(num +"es uno");
         } else if (num==2) {
-
             System.out.println(num + "es dos");
-
         } else {
             System.out.println(num +"no es ni1 ni 2 ni 3 ni 4");
-
-        }
         }
     }
-    
+}
+
